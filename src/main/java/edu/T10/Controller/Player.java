e074ec49acc0ddb0062b62c2211e1b6a818ad8f5
michael <mlyonsru@gmail.com>
2018-04-04 15:41:29 -0600

    fleshed out player

--- src/main/java/edu/T10/Controller/Player.java
+++ src/main/java/edu/T10/Controller/Player.java
@@ -1,4 +1,38 @@
 package edu.T10.Controller;
 
+import edu.T10.Model.Deck;
+import edu.T10.Model.Territory;
+
 public class Player {
+    private String id;
+    private String color; // todo should we do a color enum?
+    // private Units[] freeArmies // todo lets see if this is necessary
+    private Deck deck;
+    private Territory territories;
+
+    public Player(){
+        // todo constructor
+    }
+
+    public void playCards(){
+
+    }
+
+    public void placeUnit(Territory territory){
+
+    }
+
+    public void reinforce(){
+
+    }
+
+    public void fortify(){
+
+    }
+
+    public void invade(){ // todo changed name from attack to invade
+
+    }
+
+
 }
