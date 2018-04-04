dbf73db0e6fe3961b35dd840922f1ef838c31aa9
michael <mlyonsru@gmail.com>
2018-04-04 15:58:40 -0600

    added clarification for card type comment

--- src/main/java/edu/T10/Model/Card.java
+++ src/main/java/edu/T10/Model/Card.java
@@ -1,5 +1,5 @@
 package edu.T10.Model;
 
 public class Card {
-    private String type; // todo inheritance for different types of cards?
+    private String type; // todo enum for different types (inf, cav, art, or wild)
 }
