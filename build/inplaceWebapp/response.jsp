60ed11b7b8664b86e4be3c8a80d985fa7753a423
Charles Culpepper <charlescd7@gmail.com>
2018-04-09 12:21:33 -0600

    No React. Basic Java Servlet web app.

---
+++ build/inplaceWebapp/response.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: EddieSevenMBA
+  Date: 4/9/18
+  Time: 12:00 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Risk Test Page</title>
+</head>
+<body>
+<h1>Risk</h1>
+<h2>Welcome to Risk, ${user}!</h2>
+</body>
+</html>
--- src/main/java/edu/T10/Risk.java
+++ src/main/java/edu/T10/Risk.java
@@ -1,11 +1,30 @@
 package edu.T10;
 
-public class Risk {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-    public static void main(String[] args) {
-
-        System.out.print("Hello world!");
-
+@WebServlet(name = "Risk", urlPatterns = {"hello"}, loadOnStartup = 1)
+public class Risk extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().print("Hello, Risk bitches!!!!");
     }
+
+    protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name == null)
+            name = "World";
+        request.setAttribute("user", name);
+        request.getRequestDispatcher("response.jsp").forward(request, response);
+    }
+
+//    public static void main(String[] args) {
+//
+//        System.out.print("Hello world!");
+//
+//    }
 
 }
