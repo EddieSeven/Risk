9c4d389e0affda85a0fc742417baca4a73b840dd
“Lingyang <kathypizza330@gmail.com>
2018-04-14 16:00:13 -0600

    Game test

--- src/test/java/edu/T10/Model/TestGame.java
+++ src/test/java/edu/T10/Model/TestGame.java
@@ -1,6 +1,6 @@
 package edu.T10.Model;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,20 +9,18 @@ import org.junit.Rule;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 
 public class TestGame {
-	Game g;
 
 	@Rule
 	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
 	@Before
 	public void setUp() {	}
-	
+
 	@Test
 	public void testGame() {
 		// As private variables are not accessable outside
 		// Will use the tests that are in main to test Game for now
-		String output = "";
 		Game.main(new String[] {});
-		//assertEquals(output, systemOutRule.getLog());
+		assertTrue(systemOutRule.getLog().length() > 0);
 	}
 }
