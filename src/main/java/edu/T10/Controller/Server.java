6e9cb20784b989a6c0d60f047dad36dac1bb3627
michael <mlyonsru@gmail.com>
2018-04-13 18:03:23 -0600

    added in old server code

--- src/main/java/edu/T10/Controller/Server.java
+++ src/main/java/edu/T10/Controller/Server.java
@@ -1,83 +1,30 @@
-package edu.T10.Controller;
+package edu.T10;
 
-import edu.T10.Model.Game;
-
-import java.io.*;
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.servlet.*;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebServlet(name = "Risk", urlPatterns = {"/"}, loadOnStartup = 1)
+@WebServlet(name = "Server", urlPatterns = {"register"}, loadOnStartup = 1)
 public class Server extends HttpServlet {
-
-    private Game game;
-
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        doGetOrPost(request, response);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().print("Hello, Server!!!!");
     }
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        doGetOrPost(request, response);
+    protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name == null)
+            name = "World";
+        request.setAttribute("user", name);
+        request.getRequestDispatcher("response.jsp").forward(request, response);
     }
 
-    private void doGetOrPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("application/json");
-
-        try {
-            StringBuilder sb = new StringBuilder();
-            String s;
-            while ((s = request.getReader().readLine()) != null) {
-                sb.append(s);
-            }
-            parseCommand(sb.toString());
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            response.getOutputStream().flush();
-        }
-
-        response.setContentType("text/x-json;charset=UTF-8");
-        response.setHeader("Cache-Control", "no-cache");
-
-        response.getWriter().write("Risk WEB API \n\n");
-        return;
-    }
-
-    /*
-        Json String: request format
-     */
-    private void parseCommand(String command){
-        JsonObject json = Json.createReader(new StringReader(command)).readObject();
-        switch (json.getString("Action")) {
-            case "Init":
-                this.game = new Game(json.getInt("numOfPlayer"));
-                this.game.startGame();
-                break;
-            case "Attack":
-                this.game.conductInvasion(
-                        json.getInt("territoryID"),
-                        json.getInt("targetID"),
-                        json.getInt("unitValue"),
-                        json.getInt("attackerDice"),
-                        json.getInt("defenderDice"));
-                break;
-            case "Reinforce":
-                this.game.updateTerritory(
-                        json.getInt("territoryID"),
-                        json.getInt("unitValue"));
-                break;
-            case "PlayCards":
-                this.game.playCards(json.getInt("numOfCards"));
-                break;
-            case "EndTurn":
-                this.game.finishTurn();
-                break;
-            default:
-                break;
-        }
-    }
+//    public static void main(String[] args) {
+//
+//        System.out.print("Hello world!");
+//
+//    }
 
 }
