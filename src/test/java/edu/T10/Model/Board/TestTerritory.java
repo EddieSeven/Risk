825023880efa299ad08115b3b4dce02ce63ac10b
michael <mlyonsru@gmail.com>
2018-04-18 14:57:55 -0600

    fixed territory test

--- src/test/java/edu/T10/Model/Board/TestTerritory.java
+++ src/test/java/edu/T10/Model/Board/TestTerritory.java
@@ -7,29 +7,29 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestTerritory {
-	Territory t; // TODO FIX
+	private Territory t;
 	
 	@Before
 	public void setUp() {
 		t = new Territory();
-		//t.readTerritoryFromLine("1 TestName 0 11 12 ;");
-		//t.readAdjacentsFromLine("1 1 2 3 ;");
 	}
 	
 	@Test
-	public void testAssignGetOwner() {
-		//t.assignOwner(2);
-		//assertEquals(t.getOwner(), 2);
+	public void testAssignOwner() {
+		t.assignOwner(2);
+		assertEquals(t.getOwner(), 2);
 	}
 	
 	@Test
 	public void testGetId() {
-		//assertEquals(t.getId(), 1);
+		t.setId(1);
+		assertEquals(t.getId(), 1);
 	}
 	
 	@Test
 	public void testGetName() {
-		//assertEquals(t.getName(), "TestName");
+		t.setName("TestName");
+		assertEquals(t.getName(), "TestName");
 	}
 	
 	@Test
@@ -44,20 +44,12 @@ public class TestTerritory {
 		t.setArmyStrength(10);
 		assertEquals(t.getStrength(), 10);
 	}
-	
-	@Test
-	public void testReadTerritoryError() {
-		//assertTrue(!t.readTerritoryFromLine(""));
-	}
 
 	@Test
-	public void testReadAdjacentsError() {
-		//assertTrue(!t.readAdjacentsFromLine(""));
-	}
-	
-	@Test
 	public void testToString() {
-		//assertEquals(t.toString(), "1 0");
+		t.setId(1);
+		t.setArmyStrength(0);
+		assertEquals(t.toString(), "1 0");
 	}
 	
 }
