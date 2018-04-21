982e88f3d80fa1475912d2d10f9b77f14ba504ff
“Lingyang <kathypizza330@gmail.com>
2018-04-21 14:47:15 -0600

    Change array inti vector

--- src/main/java/edu/T10/Controller/StartGameController.java
+++ src/main/java/edu/T10/Controller/StartGameController.java
@@ -3,9 +3,7 @@ package edu.T10.Controller;
 import edu.T10.Model.Board.Board;
 import edu.T10.Model.Player;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Vector;
 
 public class StartGameController {
@@ -18,33 +16,33 @@ public class StartGameController {
 
     private void colorAssignment(Vector<Player> players){
         int len = players.size();
-        int[] assignments = intArrayShuffled(len);
+        Vector<Integer> assignments = intArrayShuffled(len);
         for (int i = 0; i < len; i++){
-            players.get(i).assignColor(assignments[i]);
+            players.get(i).assignColor(assignments.get(i));
         }
     }
 
     private void territoryAssignment(Board board, int numOfPlayers) {
         int len = (board.getTerritorySize() / numOfPlayers) * numOfPlayers;
-        int[] assignments = intArrayShuffled(len);
+        Vector<Integer> assignments = intArrayShuffled(len);
         for (int i = 0; i < len; i++) {
 
-            board.updateOwner(assignments[i] + 1, i % numOfPlayers);
+            board.updateOwner(assignments.get(i) + 1, i % numOfPlayers);
             // TODO: Default set to 3
-            board.getTerritory(assignments[i] + 1).updateArmyStrength(3);
+            board.getTerritory(assignments.get(i) + 1).updateArmyStrength(3);
         }
     }
 
-    private int[] intArrayShuffled(int size){
-        List<Integer> solution = new ArrayList<>();
+    private Vector<Integer> intArrayShuffled(int size){
+        Vector<Integer> solution = new Vector<Integer>();
         for (int i = 0; i < size; i++) {
             solution.add(i);
         }
         Collections.shuffle(solution);
-        int[] intArray = new int[solution.size()];
+        Vector<Integer> intVector = new Vector<Integer>();
         for (int i = 0; i < solution.size(); i++) {
-            intArray[i] = solution.get(i);
+            intVector.add(solution.get(i));
         }
-        return intArray;
+        return intVector;
     }
 }
