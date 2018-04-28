c041bcf890d106c0bc384058c5bcf682c6d18ad8
Michael <mlyonsru@gmail.com>
2018-04-28 12:24:44 -0600

    removed test in testGame (one that I made previously)

--- src/test/java/edu/T10/Controller/TestGame.java
+++ src/test/java/edu/T10/Controller/TestGame.java
@@ -24,20 +24,4 @@ public class TestGame {
 		// Will use the tests that are in main to test Game for now
 	}
 
-	@Test
-	public void testInvasion(){
-		Player p1 = new Player("bob", 0);
-		Player p2 = new Player("john", 1);
-		Player p3 = new Player("tony", 2);
-		Player players[] = new Player[3];
-		players[0] = p1;
-		players[1] = p2;
-		players[2] = p3;
-
-
-		Game game = new Game(players);
-		int a = 4;
-
-
-	}
 }
