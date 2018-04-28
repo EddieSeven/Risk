5bc8ec0b05437d44a4474682403e9a863dde75a6
GrEedWish <GrEedWish_x@outlook.com>
2018-04-28 14:25:28 -0600

    Remove necessities.

--- src/main/webapp/js/Helper.js
+++ src/main/webapp/js/Helper.js
@@ -46,24 +46,31 @@ function bindClick(id) {
             case "fortification":
                 if (isMyTerritory(idOnClick)) {
                     from = parseInt(idOnClick);
+                    to = 0;
                     disableInput();
                     document.getElementById("listener1").innerHTML = classes[id].title;
                     document.getElementById("listener2").innerHTML = "";
 
                     var selectList = document.createElement("select");
                     selectList.className = "custom-select";
+                    selectList.id = "fortify_select";
                     for (var i = 0; i < adjacent[from].length; i++) {
                         var curID = adjacent[from][i];
                         if (playerTerritories.indexOf(curID) != -1 && idOnClick != curID) {
                             var option = document.createElement("option");
-                            option.value = document.getElementById(curID).title;
+                            option.value = document.getElementById(curID).id;
                             option.text = document.getElementById(curID).title;
                             selectList.appendChild(option);
                         }
-
                     }
+                    selectList.addEventListener("click", function() {
+                        to = parseInt(document.getElementById("fortify_select").value);
+                        disableInput();
+                    });
                     document.getElementById("listener2").appendChild(selectList);
                 } else {
+                    document.getElementById("listener1").innerHTML = "Choose your territory";
+                    document.getElementById("listener2").innerHTML = "";
                     disableInput();
                 }
                 break;
@@ -150,9 +157,7 @@ function isMyTerritory(tid){
 
 function disableInput(){
     var input = document.getElementById("armyUnit");
-    console.log("from", from);
-    console.log("to", to);
-    if (to !=0 && adjacent[from].indexOf(to) == -1) {
+    if (to ==0 || adjacent[from].indexOf(to) == -1) {
         input.disabled = true;
     } else {
         input.disabled = false;
